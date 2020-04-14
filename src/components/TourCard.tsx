77a876a6ac7e823311662a9a3aac53c0ea1c5f2a
Gregory <grgrsm@gmail.com>
2020-04-14 13:07:25 +0300

    tour card styling

--- src/components/TourCard.tsx
+++ src/components/TourCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Img, { FluidObject } from 'gatsby-image'
 import Link from 'gatsby-plugin-transition-link/AniLink'
+import { FaMap } from 'react-icons/fa'
 
 export interface ITourCard {
   node: any
@@ -26,12 +27,16 @@ export default ({ name, country, price, days, images, slug }: ITourCard) => {
           details
         </Link>
       </div>
-      <h3 className="tour__title">{name}</h3>
       <div className="tour__content">
-        <h4 className="tour__country">{country}</h4>
-        <div className="tour__data">
-          <div className="tour__days">Days: {days}</div>
-          <div className="tour__price">From ${price}</div>
+        <h3 className="tour__title">{name}</h3>
+        <div className="tour__info">
+          <h4 className="tour__country">
+            <FaMap className="tour__icon" /> {country}
+          </h4>
+          <div className="tour__details">
+            <div className="tour__days">Days: {days}</div>
+            <div className="tour__price">From ${price}</div>
+          </div>
         </div>
       </div>
     </div>
