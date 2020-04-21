4f865bd7de5f1f10de89cffd99971294f12db005
Gregory <grgrsm@gmail.com>
2020-04-21 20:14:35 +0300

    tour card prop checking

--- src/components/TourCard.tsx
+++ src/components/TourCard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Img, { FluidObject } from 'gatsby-image'
 import Link from 'gatsby-plugin-transition-link/AniLink'
 import { FaMap } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
 export interface ITourCard {
   node: any
@@ -18,27 +20,55 @@ export interface ITourCard {
   ]
 }
 
-export default ({ name, country, price, days, images, slug }: ITourCard) => {
+const getImage = graphql`
+  query {
+    file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const Tour = ({ name, country, price, days, images, slug }: ITourCard) => {
+  const defaultImage = useStaticQuery(getImage).file.childImageSharp.fluid
+  const image = images ? images[0].fluid : defaultImage
+
   return (
     <div className="tour">
       <div className="tour__image-box">
-        <Img className="tour__image" fluid={images[0].fluid} alt="tour image" />
-        <Link fade className="tour__link" to={`/tours/${slug}`}>
+        <Img className="tour__image" fluid={image} alt="tour image" />
+        <Link fade className="tour__link" to={slug ? `/tours/${slug}` : '#'}>
           details
         </Link>
       </div>
       <div className="tour__content">
-        <h3 className="tour__title">{name}</h3>
+        <h3 className="tour__title">{name || 'unset'}</h3>
         <div className="tour__info">
           <h4 className="tour__country">
-            <FaMap className="tour__icon" /> {country}
+            <FaMap className="tour__icon" /> {country || 'unset'}
           </h4>
           <div className="tour__details">
-            <div className="tour__days">Days: {days}</div>
-            <div className="tour__price">From ${price}</div>
+            <div className="tour__days">Days: {days || 'null'}</div>
+            <div className="tour__price">From ${price || 'null'}</div>
           </div>
         </div>
       </div>
     </div>
   )
 }
+
+Tour.propTypes = {
+  // tour: PropTypes.shape({
+  // })
+  name: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  days: PropTypes.number.isRequired,
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default Tour
