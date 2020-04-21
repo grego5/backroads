da31636de8c71a54013a11e2aa2627a6c394f7f9
Gregory <grgrsm@gmail.com>
2020-04-21 21:22:42 +0300

    tour page templating

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,25 @@
 const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const { data } = await graphql(`
+    query {
+      tours: allContentfulTour {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  data.tours.edges.forEach(({ node }) => {
+    createPage({
+      path: `tours/${node.slug}`,
+      component: path.resolve('./src/templates/singleTour.tsx'),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+}
---
+++ src/templates/singleTour.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const singleTour = (props: any) => {
+  console.log(props)
+  return <div>template page</div>
+}
+
+export default singleTour
