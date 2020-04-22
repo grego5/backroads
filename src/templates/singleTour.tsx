49a38c3da000cf3f91bba5d271adcee579e117f1
grego5 <45867491+grego5@users.noreply.github.com>
2020-04-22 06:14:47 +0300

    tour page data query

--- src/templates/singleTour.tsx
+++ src/templates/singleTour.tsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-const singleTour = (props: any) => {
-  console.log(props)
-  return <div>template page</div>
+import Layout from '../components/Layout'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      description {
+        description
+      }
+      journey {
+        day
+        info
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`
+
+const singleTour = ({ data }: any) => {
+  const {
+    name,
+    price,
+    country,
+    start,
+    description: { description },
+    journey,
+    images,
+  } = data.tour
+
+  const [mainImage, ...tourImages] = images
+
+  return (
+    <Layout>
+      <h1>{name}</h1>
+
+      <Hero image={mainImage.fluid}>
+        <Banner title={name} text={start} />
+      </Hero>
+    </Layout>
+  )
 }
 
 export default singleTour
