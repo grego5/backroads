da1acc9908f16d729575d4d480dd4d17a9523803
grego5 <45867491+grego5@users.noreply.github.com>
2020-04-24 05:48:49 +0300

    tour template page styling

--- src/templates/singleTour.tsx
+++ src/templates/singleTour.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import { FaMoneyBillWave, FaMap } from 'react-icons/fa'
 
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
@@ -36,6 +37,7 @@ const singleTour = ({ data }: any) => {
     price,
     country,
     start,
+    days,
     description: { description },
     journey,
     images,
@@ -45,11 +47,34 @@ const singleTour = ({ data }: any) => {
 
   return (
     <Layout>
-      <h1>{name}</h1>
-
       <Hero image={mainImage.fluid}>
         <Banner title={name} text={start} />
       </Hero>
+      <div className="tour-layout">
+        <article className="content">
+          <div className="content__info">
+            <p className="content__text">
+              <FaMoneyBillWave className="content__icon" /> Starting from $
+              {price}
+            </p>
+
+            <p className="content__text">
+              <FaMap className="content__icon" /> {country}
+            </p>
+          </div>
+          <div className="content__title">Starts on: {start}</div>
+          <div className="content__title">Duration: {days}</div>
+          <p className="content__description">{description}</p>
+        </article>
+
+        <aside className="images">
+          {tourImages.map(({ fluid }: any, id: number) => (
+            <div className="images__box" key={id}>
+              <Img fluid={fluid} className="images__image" />
+            </div>
+          ))}
+        </aside>
+      </div>
     </Layout>
   )
 }
