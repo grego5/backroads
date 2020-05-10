99925d36655eebf79b56c72b195e8772eb11685c
Gregory <grgrsm@gmail.com>
2020-05-10 22:37:42 +0300

    sitemap and robots.txt plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,9 @@ module.exports = {
     },
     `gatsby-plugin-typescript`,
     `gatsby-plugin-playground`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
